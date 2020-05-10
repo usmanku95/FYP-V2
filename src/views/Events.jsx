9324b5c8566a9ccd3bf0efcf08bfa73c49b58223
usmanku95 <45457897+usmanku95@users.noreply.github.com>
2020-05-10 21:11:22 +0500

    sec comm

--- src/views/Events.jsx
+++ src/views/Events.jsx
@@ -3,7 +3,7 @@
 =========================================================
 * Light Bootstrap Dashboard React - v1.3.0
 =========================================================
-
+gg
 * Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
 * Copyright 2019 Creative Tim (https://www.creative-tim.com)
 * Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
@@ -17,60 +17,63 @@
 */
 import React, { Component } from "react";
 import { Grid, Row, Col, Table } from "react-bootstrap";
-import ReactTable from 'react-table-6'
-import 'react-table-6/react-table.css'
+import ReactTable from "react-table-6";
+import "react-table-6/react-table.css";
 import Card from "components/Card/Card.jsx";
 import { thArray, tdArray } from "variables/Variables.jsx";
 
 class Events extends Component {
   render() {
-    const data = [{
-        name: 'Tanner Linsley',
+    const data = [
+      {
+        name: "Tanner Linsley",
         age: 26,
-    //     friend: {
-    //       name: 'Jason Maurer',
-    //       age: 23,
-    //     }
-    //   },{
-    
-      }]
-     
-      const columns = [{
-        Header: 'Name',
-        accessor: 'name', // String-based value accessors!,
-        Cell:props => <a href='/user/matches/eventId'>{props.value}</a> // String-based value accessors!
+        //     friend: {
+        //       name: 'Jason Maurer',
+        //       age: 23,
+        //     }
+        //   },{
+      },
+    ];
 
-      }, {
-        Header: 'Year',
-        accessor: 'age',
-        Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
+    const columns = [
+      {
+        Header: "Name",
+        accessor: "name", // String-based value accessors!,
+        Cell: (props) => <a href="/user/matches/eventId">{props.value}</a>, // String-based value accessors!
+      },
+      {
+        Header: "Year",
+        accessor: "age",
+        Cell: (props) => <span className="number">{props.value}</span>, // Custom cell components!
       },
 
-     true&& {
-        Header: 'Actions',
-        accessor: 'age',
-        Cell: props => <span className='number'><button className='btn btn-sm btn-primary btn-fill'>edit</button> <button className='btn btn-sm btn-danger btn-fill'>delete</button></span> // Custom cell components!
+      true && {
+        Header: "Actions",
+        accessor: "age",
+        Cell: (props) => (
+          <span className="number">
+            <button className="btn btn-sm btn-primary btn-fill">edit</button>{" "}
+            <button className="btn btn-sm btn-danger btn-fill">delete</button>
+          </span>
+        ), // Custom cell components!
       },
-    //    {
-    //     id: 'friendName', // Required because our accessor is not a string
-    //     Header: 'Friend Name',
-    //     // accessor: d => d.friend.name // Custom value accessors!
-    //   }, {
-    //     Header: props => <span>Friend Age</span>, // Custom header components!
-    //     accessor: 'friend.age'
-    //   }
-]
-     
-      
+      //    {
+      //     id: 'friendName', // Required because our accessor is not a string
+      //     Header: 'Friend Name',
+      //     // accessor: d => d.friend.name // Custom value accessors!
+      //   }, {
+      //     Header: props => <span>Friend Age</span>, // Custom header components!
+      //     accessor: 'friend.age'
+      //   }
+    ];
+
     return (
       <div className="content">
         <Grid fluid>
           <Row>
-            <button className='btn btn-info btn-fill'>+ Add Event</button>
-          <ReactTable
-    data={data}
-    columns={columns}
-  />
+            <button className="btn btn-info btn-fill">+ Add Event</button>
+            <ReactTable data={data} columns={columns} />
           </Row>
         </Grid>
       </div>
